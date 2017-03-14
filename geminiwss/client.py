c2e410f2285cbd7bde14fc7c4ab047887e134b20
nlsdfnbch <23okrs35+github@mykolab.com>
2017-03-14 15:39:32 +0100

    moved code to be more logical

--- geminiwss/client.py
+++ geminiwss/client.py
@@ -25,24 +25,30 @@ class GeminiWss:
 
         self.addr = 'wss://api.gemini.com/v1/'
 
+    def _subscription_thread(self, endpoint):
+        """
+        Thread Method, running the connection for each endpoint.
+        :param endpoint:
+        :return:
+        """
+        conn = create_connection(self.addr + endpoint, timeout=1)
+
+        while self.running[endpoint]:
+            try:
+                msg = conn.recv()
+            except WebSocketTimeoutException:
+                raise
+
+            self.endpoint_qs[endpoint].put(msg)
+
     def start(self):
         self.running = True
         for endpoint in self.endpoints:
-            self.running[endpoint] = True
-            t = Thread(target=self._subscription_thread,
-                       args=(endpoint,))
-            t.daemon = True
-            t.start()
-            self.endpoint_threads[endpoint] = t
+            self.subscribe(endpoint)
 
     def stop(self):
         for endpoint in self.endpoints:
-            self.running[endpoint] = False
-            self.endpoint_threads[endpoint].join()
-            while not self.endpoint_qs[endpoint].empty:
-                time.sleep(.5)
-            self.endpoint_qs.pop(endpoint)
-
+            self.unsubscribe(endpoint)
         self.garbage_collector()
 
     def restart(self):
@@ -50,7 +56,23 @@ class GeminiWss:
         self.start()
 
     def subscribe(self, endpoint):
-        return create_connection(self.addr + endpoint, timeout=1)
+        self.running[endpoint] = True
+        t = Thread(target=self._subscription_thread,
+                   args=(endpoint,))
+        t.daemon = True
+        t.start()
+        self.endpoint_threads[endpoint] = t
+
+    def unsubscribe(self, endpoint):
+        self.running[endpoint] = False
+        self.endpoint_threads[endpoint].join()
+        while not self.endpoint_qs[endpoint].empty():
+            time.sleep(1)
+
+        self.endpoint_threads.pop(endpoint)
+        self.running.pop(endpoint)
+        if endpoint in self.endpoint_qs:
+            self.endpoint_qs.pop(endpoint)
 
     def garbage_collector(self):
         for endpoint in self.endpoints:
@@ -70,26 +92,7 @@ class GeminiWss:
                 except KeyError:
                     pass
 
-    def unsubscribe(self, endpoint):
-        self.running[endpoint] = False
-        self.endpoint_threads[endpoint].join()
-        while not self.endpoint_qs[endpoint].empty():
-            time.sleep(1)
 
-        self.endpoint_threads.pop(endpoint)
-        self.running.pop(endpoint)
-        self.endpoint_qs.pop(endpoint)
-
-    def _subscription_thread(self, endpoint):
-        conn = self.subscribe(endpoint)
-
-        while self.running[endpoint]:
-            try:
-                msg = conn.recv()
-            except WebSocketTimeoutException:
-                raise
-
-            self.endpoint_qs[endpoint].put(msg)
 
 
 
