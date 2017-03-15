537a01e3797d1413a9a879a9f849344ed613de9c
nlsdfnbch <23okrs35+github@mykolab.com>
2017-03-15 10:01:42 +0100

    added failsafe for connection errors during setup

--- geminiwss/client.py
+++ geminiwss/client.py
@@ -22,8 +22,24 @@ class GeminiWss:
         self.endpoint_threads = {}
         self.running = {}
         self.endpoint_qs = defaultdict(Queue)
-
+        self.restarter_thread = None
+        self.restart_q = Queue()
         self.addr = 'wss://api.gemini.com/v1/'
+
+    def _restart_thread(self):
+        """
+        Restarts subscription threads if their connection drops.
+        :return:
+        """
+        while self.running['restart_thread']:
+            try:
+                endpoint = self.restart_q.get(timeout=.5)
+            except TimeoutError:
+                continue
+            log.info("_restart_thread(): Restarting Thread for endpoint %s",
+                     endpoint)
+            self.unsubscribe(endpoint)
+            self.subscribe(endpoint)
 
     def _subscription_thread(self, endpoint):
         """
@@ -31,22 +47,32 @@ class GeminiWss:
         :param endpoint:
         :return:
         """
-        conn = create_connection(self.addr + endpoint, timeout=1)
+        try:
+            conn = create_connection(self.addr + endpoint, timeout=1)
+        except WebSocketTimeoutException:
+            self.restart_q.put(endpoint)
+            return
 
         while self.running[endpoint]:
             try:
                 msg = conn.recv()
             except WebSocketTimeoutException:
-                raise
+                self.restart_q.put(endpoint)
 
             self.endpoint_qs[endpoint].put(msg)
 
     def start(self):
-        self.running = True
+        self.restarter_thread = Thread(target=self._restart_thread,
+                                       name='Restarter Thread')
+        self.restarter_thread.daemon = True
+        self.running['restart_thread'] = True
+        self.restarter_thread.start()
         for endpoint in self.endpoints:
             self.subscribe(endpoint)
 
     def stop(self):
+        self.running['restart_thread'] = False
+        self.restarter_thread.join()
         for endpoint in self.endpoints:
             self.unsubscribe(endpoint)
         self.garbage_collector()
@@ -58,7 +84,7 @@ class GeminiWss:
     def subscribe(self, endpoint):
         self.running[endpoint] = True
         t = Thread(target=self._subscription_thread,
-                   args=(endpoint,))
+                   args=(endpoint,), name=endpoint)
         t.daemon = True
         t.start()
         self.endpoint_threads[endpoint] = t
@@ -66,31 +92,16 @@ class GeminiWss:
     def unsubscribe(self, endpoint):
         self.running[endpoint] = False
         self.endpoint_threads[endpoint].join()
-        while not self.endpoint_qs[endpoint].empty():
-            time.sleep(1)
 
-        self.endpoint_threads.pop(endpoint)
-        self.running.pop(endpoint)
-        if endpoint in self.endpoint_qs:
-            self.endpoint_qs.pop(endpoint)
+        self.garbage_collector()
 
     def garbage_collector(self):
         for endpoint in self.endpoints:
-            # check all caches for dead threads and clean up
             if self.endpoint_threads[endpoint].isalive():
                 continue
             else:
                 self.endpoint_threads.pop(endpoint)
-                while not self.endpoint_qs[endpoint].empty():
-                    time.sleep(0.5)
 
-                if endpoint in self.endpoint_qs:
-                    self.endpoint_qs.pop(endpoint)
-
-                try:
-                    self.running.pop(endpoint)
-                except KeyError:
-                    pass
 
 
 
